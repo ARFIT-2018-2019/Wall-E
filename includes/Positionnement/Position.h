7549ebd28e0791c3d15dc665312ae9e4483fbdff
Ice2o <alexandre.fauchard41@gmail.com>
2018-11-29 15:21:53 +0100

    Ajout de la classe pour la position

--- includes/Positionnement/Position.h
+++ includes/Positionnement/Position.h
@@ -5,4 +5,24 @@
 #ifndef WALL_E_POSITION_H
 #define WALL_E_POSITION_H
 
+#include <Arduino.h>
+#include "../SerialCom.h"
+#include <SoftwareSerial.h>
+#include <RoboClaw.h>
+#include "EnumMotor.h"
+
+class Position {
+
+public:
+    Position();
+
+    void init();
+
+    int getPosX();
+    int getPosY();
+
+private:
+
+};
+
 #endif //WALL_E_POSITION_H
