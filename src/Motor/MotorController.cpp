0027bb876cc69a901d2e87fa9ee130a66fdcba48
Ice2o (Alexandre) <alexandre.fauchard41@gmail.com>
2019-03-14 15:56:21 +0100

    Ajout début odometrie

--- src/Motor/MotorController.cpp
+++ src/Motor/MotorController.cpp
@@ -3,6 +3,8 @@
 //
 
 #include "../../includes/Motor/MotorController.h"
+#include <Arduino.h>
+
 //==================================================
 //================== CONSTRUCTORS ==================
 //==================================================
@@ -36,6 +38,41 @@ void MotorController::allerA(Position position) {
         roboClaw->ForwardBackwardM2(MOTOR_ADRESSE, cmd[1]);
     }*/
     //TODO Avancer
+
+    Position* positionActuelle =  this->odometrie.getPositionRobot();
+
+    double xDebut = positionActuelle->getX();
+    double yDebut = positionActuelle->getY();
+    double angleDebut = positionActuelle->getAngle();
+
+    double xCible = positionActuelle->getX();
+    double yCible = positionActuelle->getY();
+    double angleCible = positionActuelle->getAngle();
+
+    double distX = positionActuelle->getX();
+    double distY = positionActuelle->getY();
+    double distAngle = positionActuelle->getAngle();
+
+    //TODO Calcul angleAvantDeplacementCible
+    double scal = yCible;
+    double norme = sqrt((xCible * xCible) + (yCible * yCible));
+
+    double anglePourDeplacement = acos(scal / norme);
+    double angleAvantDeplacement = CalculAngleRotation(angleDebut, anglePourDeplacement);
+
+    //TODO Calcul angleRotationAvantDeplacement
+
+
+    //TODO Calcul angleRotationApresDeplacement
+
+    //TODO Calcul distance
+
+    //TODO Tourner
+
+    //TODO Avancer
+
+    //TODO Tourner
+
 }
 
 void MotorController::tournerDe(float deg) {
@@ -49,5 +86,88 @@ void MotorController::stop(){
 
 Odometrie MotorController::getOdometrie(){
     return odometrie;
+}
+
+private double CalculAngleRotation (double angle1, double angle2)
+{
+    double angleRotation = 0;
+    if(angle1 != angle2)
+    {
+        if(angle2 < angle1) {
+            if (angle1 <= 180) {
+                return angle2 - angle1;
+            } else {
+                if (angle1 - 180 <= angle2) {
+                    return angle2 - angle1;
+                } else {
+                    return -((360 + angle2) - angle1);
+                }
+            }
+        }else{
+            if(angle2 - 180 <= angle1)
+            {
+                return angle2 - angle1;
+            }else{
+                return -((360 + angle1) - angle2);
+            }
+        }
+    }
+
+    return angleRotation;
+}
+
+private double asin(double c)
+{
+
+    double out;
+
+    out= ((c+(c*c*c)/6+(3*c*c*c*c*c)/40+(5*c*c*c*c*c*c*c)/112+
+
+           (35*c*c*c*c*c*c*c*c*c)/1152 +(c*c*c*c*c*c*c*c*c*c*c*0.022)+
+
+           (c*c*c*c*c*c*c*c*c*c*c*c*c*.0173)+(c*c*c*c*c*c*c*c*c*c*c*c*c*c*c*.0139)+
+
+           (c*c*c*c*c*c*c*c*c*c*c*c*c*c*c*c*c*0.0115)+(c*c*c*c*c*c*c*c*c*c*c*c*c*c*c*c*c*c*c*0.01)
+
+    ));
+
+    //asin
+
+    if(c>=.96 && c<.97){
+        out=1.287+(3.82*(c-.96));
+    }
+
+    if(c>=.97 && c<.98){
+        out=(1.325+4.5*(c-.97));
+    }
+
+    if(c>=.98 && c<.99){
+        out=(1.37+6*(c-.98));
+    }
+
+    if(c>=.99 && c<=1){
+        out=(1.43+14*(c-.99));
+    }
+
+    return out;
+}
+
+private double acos(double c)
+{
+    double out;
+
+    out=asin(sqrt(1-c*c));
+
+    return out;
+}
+
+private double atan(double c)
+
+{
+    double out;
+
+    out=asin(c/(sqrt(1+c*c)));
+
+    return out;
 }
 
--- src/Motor/Odometrie.cpp
+++ src/Motor/Odometrie.cpp
@@ -18,131 +18,131 @@ Odometrie::Odometrie(Position* positionRobot){
 }
 
 void Odometrie::init(){
-	this->erreurPre = 0.0;
+    this->erreurPre = 0.0;
 }
 
 
 void Odometrie::updateCodeurGauche(){
     if(digitalRead(ENCODEUR_ROUE_CODEUSE_GAUCHE_VA) == digitalRead(ENCODEUR_ROUE_CODEUSE_GAUCHE_VB))
-		CountEncodeurG--;        
-	else
-		CountEncodeurG++;            
+        CountEncodeurG--;
+    else
+        CountEncodeurG++;
 }
 
 void Odometrie::updateCodeurDroit(){
     if(digitalRead(ENCODEUR_ROUE_CODEUSE_DROITE_VA) ==  digitalRead(ENCODEUR_ROUE_CODEUSE_DROITE_VB))
-		CountEncodeurD--;        
-	else
-		CountEncodeurD++;   
+        CountEncodeurD--;
+    else
+        CountEncodeurD++;
 }
 
 
 void Odometrie::calcPWM(Position positionCible, int *cmdReturn){
     double coeffP = 5.0;
-	double coeffD = 3.0;
-	int cmdD = 0, cmdG = 0;
-	int signe=0;
+    double coeffD = 3.0;
+    int cmdD = 0, cmdG = 0;
+    int signe=0;
     double dDist = 0.0, dAngl = 0.0;
-	double distanceCible=0.0, consigneOrientation=0.0;
+    double distanceCible=0.0, consigneOrientation=0.0;
 
     double erreurAngle = 0.0;
-	double  deltaErreur = 0.0;
+    double  deltaErreur = 0.0;
 
     //On commence par calculer les variations de position en distance et en angle
 
-	dDist = (COEFFGLONG*comptG + COEFFDLONG*comptD)/2.0;
-	dAngl = COEFFDANGL*comptD - COEFFGANGL*comptG;
+    dDist = (COEFFGLONG*comptG + COEFFDLONG*comptD)/2.0;
+    dAngl = COEFFDANGL*comptD - COEFFGANGL*comptG;
 
-	//Actualisation de la position du robot en xy et en orientation
-	positionRobot.setX(positionRobot.getX() +dDist*cos(dAngl));
-	positionRobot.setY(positionRobot.getY() +dDist*sin(dAngl));
-	positionRobot.setAngle(positionRobot.getAngle() +dAngl);
+    //Actualisation de la position du robot en xy et en orientation
+    positionRobot.setX(positionRobot.getX() +dDist*cos(dAngl));
+    positionRobot.setY(positionRobot.getY() +dDist*sin(dAngl));
+    positionRobot.setAngle(positionRobot.getAngle() +dAngl);
 
-	//On calcule la distance séparant le robot de sa cible
-	distanceCible = sqrt(
-		(positionCible.getX()-positionRobot.getX())*(positionCible.getX()-positionRobot.getX())+
-		(positionCible.getY()-positionRobot.getY())*(positionCible.getY()-positionRobot.getY())
-	);
+    //On calcule la distance séparant le robot de sa cible
+    distanceCible = sqrt(
+            (positionCible.getX()-positionRobot.getX())*(positionCible.getX()-positionRobot.getX())+
+            (positionCible.getY()-positionRobot.getY())*(positionCible.getY()-positionRobot.getY())
+    );
 
-	//On regarde si la cible est à gauche ou à droite du robot
-	if(positionRobot.getY() > positionCible.getY())
-		signe = 1;
-	else
-		signe = -1;
-	
-	//On calcule l'angle entre le robot et la cible
-	consigneOrientation = signe * acos(
-		((positionCible.getX()-positionRobot.getX())/(positionCible.getX()-positionRobot.getX()))*
-		((positionCible.getX()-positionRobot.getX())*(positionCible.getY()-positionRobot.getY()))*
-		(positionCible.getY()-positionRobot.getY())
-	);
-						
-	//On détermine les commandes à envoyer aux moteurs
-	cmdD = abs((int)distanceCible);
-	if(cmdD>255)
-	{
-	  	cmdD = 255;
-	}
-	cmdG = cmdD;
-									   
-	//Calcul de l'erreur
-	erreurAngle =  consigneOrientation - positionRobot.getAngle();
-									   
-	//Calcul de la différence entre l'erreur au coup précédent et l'erreur actuelle.
-	deltaErreur = erreurAngle - erreurPre;
+    //On regarde si la cible est à gauche ou à droite du robot
+    if(positionRobot.getY() > positionCible.getY())
+        signe = 1;
+    else
+        signe = -1;
 
-	//Mise en mémoire de l'erreur actuelle
-	erreurPre = erreurAngle;
-				
-	//Calcul de la valeur à envoyer aux moteurs pour tourner
-	int deltaCommande = coeffP*erreurAngle + coeffD * deltaErreur;
-									   
-	cmdG += deltaCommande;
-	cmdD -= deltaCommande;
-									   
-	if(cmdD>255)
-	{
-	  	cmdD = 255;
-	}else if(cmdD < 0)
-	{
-	  	cmdD = 0;
-	}
-									   
-	if(cmdG>255)
-	{
-	  	cmdG = 255;
-	}else if(cmdG < 0)
-	{
-	  	cmdG = 0;
-	}
-	
-	cmdReturn[0] = cmdD;
-	cmdReturn[1] = cmdG;
+    //On calcule l'angle entre le robot et la cible
+    consigneOrientation = signe * acos(
+            ((positionCible.getX()-positionRobot.getX())/(positionCible.getX()-positionRobot.getX()))*
+            ((positionCible.getX()-positionRobot.getX())*(positionCible.getY()-positionRobot.getY()))*
+            (positionCible.getY()-positionRobot.getY())
+    );
+
+    //On détermine les commandes à envoyer aux moteurs
+    cmdD = abs((int)distanceCible);
+    if(cmdD>255)
+    {
+        cmdD = 255;
+    }
+    cmdG = cmdD;
+
+    //Calcul de l'erreur
+    erreurAngle =  consigneOrientation - positionRobot.getAngle();
+
+    //Calcul de la différence entre l'erreur au coup précédent et l'erreur actuelle.
+    deltaErreur = erreurAngle - erreurPre;
+
+    //Mise en mémoire de l'erreur actuelle
+    erreurPre = erreurAngle;
+
+    //Calcul de la valeur à envoyer aux moteurs pour tourner
+    int deltaCommande = coeffP*erreurAngle + coeffD * deltaErreur;
+
+    cmdG += deltaCommande;
+    cmdD -= deltaCommande;
+
+    if(cmdD>255)
+    {
+        cmdD = 255;
+    }else if(cmdD < 0)
+    {
+        cmdD = 0;
+    }
+
+    if(cmdG>255)
+    {
+        cmdG = 255;
+    }else if(cmdG < 0)
+    {
+        cmdG = 0;
+    }
+
+    cmdReturn[0] = cmdD;
+    cmdReturn[1] = cmdG;
 }
 
 void Odometrie::reccurentTask(){
-  //Fonction executée toutes les TECOMMANDE ms sur interruption.
-  // Afin de faciliter l'odométrie, veuillez mettre en commentaire la commande des moteurs dans la boucle asservissement
+    //Fonction executée toutes les TECOMMANDE ms sur interruption.
+    // Afin de faciliter l'odométrie, veuillez mettre en commentaire la commande des moteurs dans la boucle asservissement
 
-	int CountEncodeurCGTempsCourant = CountEncodeurG - CountEncodeurCGTempsPrecedent;
-  	int CountEncodeurCDTempsCourant = CountEncodeurD - CountEncodeurCDTempsPrecedent;
-  
-  
-	float DRG = (3.1416*DIAMETRE_ROUE_CODEUSE*CountEncodeurCGTempsCourant)/1024.0000;
-	float DRD = (3.1416*DIAMETRE_ROUE_CODEUSE*CountEncodeurCDTempsCourant)/1024.0000;
-	float DC = (DRG+DRD)*0.5;
-	
-	float xtETdt = xt + DC*cos(thetat);
-	float ytETdt = yt + DC*sin(thetat);
-	float thetatETdt = (thetat + ((DRD-DRG)/ENTRAXE_ROUE_CODEUSE));
+    int CountEncodeurCGTempsCourant = CountEncodeurG - CountEncodeurCGTempsPrecedent;
+    int CountEncodeurCDTempsCourant = CountEncodeurD - CountEncodeurCDTempsPrecedent;
 
-	this->positionRobot->setX(xtETdt);
-	this->positionRobot->setX(ytETdt);
 
-	this->positionRobot->setAngle(thetatETdt);
-	
-	CountEncodeurCGTempsPrecedent = CountEncodeurG;
-	CountEncodeurCDTempsPrecedent = CountEncodeurD;
+    float DRG = (3.1416*DIAMETRE_ROUE_CODEUSE*CountEncodeurCGTempsCourant)/1024.0000;
+    float DRD = (3.1416*DIAMETRE_ROUE_CODEUSE*CountEncodeurCDTempsCourant)/1024.0000;
+    float DC = (DRG+DRD)*0.5;
+
+    float xtETdt = xt + DC*cos(thetat);
+    float ytETdt = yt + DC*sin(thetat);
+    float thetatETdt = (thetat + ((DRD-DRG)/ENTRAXE_ROUE_CODEUSE));
+
+    this->positionRobot->setX(xtETdt);
+    this->positionRobot->setX(ytETdt);
+
+    this->positionRobot->setAngle(thetatETdt);
+
+    CountEncodeurCGTempsPrecedent = CountEncodeurG;
+    CountEncodeurCDTempsPrecedent = CountEncodeurD;
 }
 
 //==================================================
